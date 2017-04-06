8f4e523f8b4afc5f8fce51ffbb72fdb49c305ca7
Aniket Muruskar <aniket23muruskar@gmail.com>
2017-04-06 10:11:14 +0530

    Form added, dependencies install, configuration done

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# angular-validation
-This repository demonstrate the basic validation in angularjs
+# Simple Angular-Validation using custom directives
+
+
+## Overview
+
+This repository demonstrates the simple form validation  in web-application using
+AngularJS.
+
+
+## Prerequisites
+
+### Git
+
+- A good place to learn about setting up git is [here][git-setup].
+- You can find documentation and download git [here][git-home].
+
+### Node.js and Tools
+
+- Get [Node.js][node-download].
+- Install the tool dependencies: `npm install`
+
+### Bower
+- Install [Bower][bower] globally.
+
+### Running the Application during Development
+
+- Run `npm start`.
+- Navigate your browser to [http://localhost:8000/](http://localhost:8000/) to see the application 
+- running.
+
+
+## Workings of the Application
+
+- The app is having only single page form i.e `index.html` in app folder
+- This app use simple local static web server i.e Http-Server
+- There is no dynamic backend (no application server) for this app. Instead we use the
+  static JSON files for validation which require server validation i.e unique email address.
+- This app using [ngMessages][angular-messages] modules for displaying messages within templates.
+
+
+## Application Directory Layout
+
+```
+app/                     --> all the source code of the app
+  bower_components/...   --> 3rd party JS/CSS libraries, including Angular and Angular Messages
+  angular-validator/
+  	users.json  		 --> already register email address
+  	validator.module.js  --> the main validator module, here we can extra validation
+  app.module.js          --> the main app module
+  index.html             --> app layout file (the main HTML template file of the app)
+
+node_modules/...         --> development tools (fetched using `npm`)
+
+bower.json               --> Bower specific metadata, including client-side dependencies
+package.json             --> Node.js specific metadata, including development tools dependencies
+.bowerrc				 --> This file specify directory where to place install front end libraries using bower, 
+							 by default within `app/bower_components/`
+
+
+
+## Contact
+
+For more information on AngularJS, please check out https://angularjs.org/.
+
+[bower]: http://bower.io/
+[git-home]: https://git-scm.com
+[git-setup]: https://help.github.com/articles/set-up-git/
+[node-download]: https://nodejs.org/en/download/package-manager/
+[angular-messages]: https://docs.angularjs.org/api/ngMessages
--- app/angular-validator/validator.module.js
+++ app/angular-validator/validator.module.js
@@ -1,6 +1,93 @@
-'use strict';
-
-// Define the `validator` module
-angular.module('validator', [
-  'ngMessages',
-]);
+;(function () {
+    'use strict';
+	// Define the `validator` module
+	angular.module('validator', [
+	  'ngMessages',
+	])
+	.directive("uniqueEmail", ['$q', '$http', function ($q, $http) {
+	    return {
+	        restrict: "A",
+	        require: "ngModel",
+	        link: function (scope, element, attrs, controller) {
+	            
+	            controller.$asyncValidators.verify = function (modelValue, viewValue) {
+	                
+	                var deferred = $q.defer();
+	                var url = document.location.origin + 'app/angular-validator/users.json';
+	                
+	                $http.get(url, viewValue).then(function (response) {
+	                    if(response.data.result) {
+	                        deferred.reject();
+	                    } else {
+	                       deferred.resolve(); 
+	                    }
+	                }, function(response) {
+	                    deferred.reject();
+	                });
+	                return deferred.promise;
+	            };
+	        }
+	    };
+	}])
+	.directive("alphaInput", function() {
+	    return {
+	        restrict: "A",
+	        require: "ngModel",
+	        link: function (scope, element, attrs, controller) {
+	            controller.$validators.alphabet = function (value) {
+	                return /^[a-zA-Z ]*$/.test(value); 
+	            };
+	        }
+	    };
+	})
+	.directive("mobileInput", function() {
+	    return {
+	        restrict: "A",
+	        require: "ngModel",
+	        link: function (scope, element, attrs, controller) {
+	            controller.$validators.contact = function (value) {
+	               return /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(value);               
+	            };
+	        }
+	    };
+	})
+	.directive("compareTo", function() {
+	    return {
+	        restrict: "A",
+	        require: "ngModel",
+	        scope:{
+	          otherModelValue:"=compareTo"  
+	        },
+	        link: function (scope, element, attrs, controller) {
+	            controller.$validators.compare = function (value) {
+	                return scope.otherModelValue == value;
+	            };
+	            scope.$watch("otherModelValue", function () {
+	                controller.$validate();
+	            });
+	        }
+	    };
+	})
+	.directive("priceInput", function() {
+	    return {
+	        restrict: "A",
+	        require: "ngModel",
+	        link: function (scope, element, attrs, controller) {
+	            controller.$validators.price = function (value) {
+	                return /^(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/.test(value); 
+	            };
+	        }
+	    };
+	})
+	.directive("pincodeInput", function() {
+	    return {
+	        restrict: "A",
+	        require: "ngModel",
+	        link: function (scope, element, attrs, controller) {
+	            controller.$validators.pincode = function (value) {
+	                return /^([1-9])([0-9]){5}$/.test(value);
+	            };
+	        }
+	    };
+	});
+})();
